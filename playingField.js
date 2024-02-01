4b898509aba01e72e49f5ab759149db919cf6fec
Melanie Herter <herter@otris.de>
2024-02-01 12:05:13 +0100

    gamepage, layout, button

---
+++ playingField.js
@@ -0,0 +1,14 @@
+const buttonElement = document.getElementById('start-button');
+const imgElement = document.getElementById('imgPlayingField');
+
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(imgElement);
+});
